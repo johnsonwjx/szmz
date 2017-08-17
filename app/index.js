cb11b378f0fc7a1b06c8ad24fc93c4ed882d9277
johnson <wujiaxiong0@gmail.com>
2017-08-18 03:12:36 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,16 +1,4 @@
 require('purecss');
 require("font-awesome/scss/font-awesome.scss");
 require('./scss/app.scss');
-
-var printMe = require('./print.js');
-
-// function component() {
-//     var element = document.createElement('div');
-//     var btn = document.createElement('button');
-//     btn.innerHTML = 'clickMe';
-//     btn.onclick = printMe;
-//     element.appendChild(btn);
-//     return element;
-// }
-//
-// document.body.appendChild(component());
+var $=require('jquery');
--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,93 @@
 var
-    path = require("path"),
-    webpack = require('webpack'),
-    src = path.resolve(__dirname, 'app'),
-    dist = path.resolve(__dirname, '../web/szmz');
+  path = require("path"),
+  webpack = require('webpack'),
+  src = path.resolve(__dirname, 'app'),
+  dist = path.resolve(__dirname, 'dist');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-    entry: {
-        "index": src + '/index.js'
-    },
-    output: {
-        path: dist,
-        filename: "[name].js"
-    },
-    resolve: {
-        root: src,
-        alias: {
-            jquery: src + '/lib/jquery.min.js'
-        }
-    },
-    module: {
-        //加载器配置
-        loaders: [{
-            include: require.resolve(src + '/lib/jquery.min.js'),
-            loader: 'exports-loader?window.jQuery'
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-        }, {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-        }, {
-            test: /\.html$/,
-            loader: "mustache-loader"
-        }, {
-            test: /\.(png|jpg)$/,
-            loader: 'url-loader?limit=8192'
-        }, {
-            test: /\.(eot|svg|ttf|woff|woff2|png)\w*/,
-            loader: 'file'
-        }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            chunks: ['index'],
-            template: src + '/index.html',
-            filename: dist + '/index.html',
-            hash: true
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        }),
-        new ExtractTextPlugin("[name].css")
-    ]
+  entry: {
+    "index": src + '/index.js'
+  },
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  resolve: {
+    root: src,
+    alias: {
+      jquery: src + '/lib/jquery.min.js'
+    }
+  },
+  module: {
+    //加载器配置
+    loaders: [{
+      include: require.resolve(src + '/lib/jquery.min.js'),
+      loader: 'exports-loader?window.jQuery'
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+    }, {
+      test: /\.html$/,
+      loader: "html-loader"
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: 'url-loader?limit=8192'
+    }, {
+      test: /\.(eot|svg|ttf|woff|woff2|png)\w*/,
+      loader: 'file'
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['index'],
+      template: src + '/index.html',
+      filename: dist + '/index.html',
+      hash: true
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new ExtractTextPlugin("[name].css")
+  ]
 
 };
 
 if (process.env.NODE_ENV !== 'production') {
-    module.exports.devtool = 'eval-source-map'; //配置生成Source Maps，选择合适的选项
-    module.exports.devServer = {
-        host: '0.0.0.0',
-        port: 8888,
-        contentBase: dist, //本地服务器所加载的页面所在的目录
-        colors: true, //终端中输出结果为彩色
-        hot: true,
-        inline: true,
-        progress: true
-    }
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.HotModuleReplacementPlugin() //热加载插件
-    ]);
+  module.exports.devtool = 'eval-source-map'; //配置生成Source Maps，选择合适的选项
+  module.exports.devServer = {
+    host: '0.0.0.0',
+    port: 8888,
+    contentBase: dist, //本地服务器所加载的页面所在的目录
+    colors: true, //终端中输出结果为彩色
+    hot: true,
+    inline: true,
+    progress: true
+  }
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.HotModuleReplacementPlugin() //热加载插件
+  ]);
 }
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map';
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin()
-    ])
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ])
 }
