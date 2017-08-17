ded12b9b465af8355819c3324a132a3645e18dc1
wujiaxiong <wujiaxiong@youngfriend.cn>
2017-08-17 17:45:32 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,16 @@
+require('purecss');
+require("font-awesome/scss/font-awesome.scss");
+require('./scss/app.scss');
+
+var printMe = require('./print.js');
+
+// function component() {
+//     var element = document.createElement('div');
+//     var btn = document.createElement('button');
+//     btn.innerHTML = 'clickMe';
+//     btn.onclick = printMe;
+//     element.appendChild(btn);
+//     return element;
+// }
+//
+// document.body.appendChild(component());
