3ce199f16ffea16ef917ed3f7bdc37606a2d0e6d
wujiaxiong <wujiaxiong@youngfriend.cn>
2017-08-22 15:03:44 +0800

    - 登录用户信息 - 退出系统

--- app/index.js
+++ app/index.js
@@ -2,12 +2,26 @@ require('purecss');
 require("font-awesome/scss/font-awesome.scss");
 require('./scss/app.scss');
 require('./tab/tab.js');
-require('./sidebar/sidebar.js')
-require('./navbar/navbar.js')
-require('./news/news.js')
-require('./message/message.js')
-require('./finance/finance.js')
-require('./contact/contact.js')
-require('./calendar/calendar.js')
-require('./task/task.js')
-module.exports = {}
+require('./sidebar/sidebar.js');
+require('./navbar/navbar.js');
+require('./news/news.js');
+require('./message/message.js');
+require('./finance/finance.js');
+require('./contact/contact.js');
+require('./calendar/calendar.js');
+require('./task/task.js');
+$.getJSON('business.do?action=getUserInfo', function(userInfo) {
+  $('#welcome h4').html('欢迎您:' + userInfo.username + ' 角色:管理员');
+  window.userInfo = userInfo;
+}).fail(function(response) {
+  if (response.responseText === '未登录') {
+    window.location.href = Common.rootpath;
+  }
+});
+
+function reLogin() {
+  window.location.href = Common.rootpath + 'login.do?action=relogin';
+}
+$('#close-system').click(reLogin);
+
+module.exports = {};
