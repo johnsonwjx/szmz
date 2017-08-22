e62ea0f741008c9e0eabd744eb1955254b701f88
wujiaxiong <wujiaxiong@youngfriend.cn>
2017-08-22 08:09:45 +0800

    左树添加点击打开功能

--- app/sidebar/sidebar.js
+++ app/sidebar/sidebar.js
@@ -31,10 +31,165 @@ $.getJSON(url, function(funcs) {
     var $node = $(this);
     var url = $node.attr('url');
     if (url) {
-      window.open(Common.rootpath + url);
+      openFlowUrl(Common.rootpath + url, 1);
     }
   });
   $('#sidebar .tree .level1 .icon').each(function(index, item) {
     $(item).addClass(icons[index])
   });
 })
+
+/**
+ * 新窗口打开或者直接打开
+ * @param url 路径后面的参数
+ * @param openMode 打开方式，0 或1,表示页面打开模式是非打开新页面和打开新页面
+ */
+function openFlowUrl(url, openMode) {
+
+  openMode = "1";
+
+  //新窗口打开
+  if (openMode == "1") {
+
+    //计算窗口的打开位置为居中
+    var left = (1024 - 780) / 2;
+    var top = (768 - 550) / 2;
+    try {
+      left = (window.screen.width - 780) / 2;
+      top = (window.screen.height - 550) / 2;
+    } catch (e) {}
+    //检查业务功能是不是工作流业务，如果是直接跳转
+    //将&amp;换成&
+    url = url.replace(/&amp;/gi, "&");
+    var funcType = url.substring(url.indexOf("&funcType=") + 10);
+    funcType = funcType.substring(0, funcType.indexOf("&"));
+
+    var funcIdStr = url.substring(url.indexOf("&funcId=") + 8);
+    funcIdStr = funcIdStr.substring(0, funcIdStr.indexOf("&"));
+
+    var size = "width=780,height=550,left=" + left + ",top=" + top + ",";
+    if ("1" == funcType) {
+      var code = url.substring(url.indexOf("&flowClassID=") + 13);
+      code = code.substring(0, code.indexOf("&"));
+      size = getStyleWidthHeight(code, "", "", "");
+      if (size != null) url += "&rs=0&mt=0"; //已经调整了，不在需要调整大小
+      else size = "width=780,height=550,left=0,top=0,";
+
+      //取出宽度和高度，然后调整为居中
+      var wVal = size.substring(size.indexOf("width=") + 6, size.indexOf(",height="));
+      var hVal = size.substring(size.indexOf("height=") + 7, size.indexOf(",left="));
+
+      var leftVa = 0;
+      var topVa = 0;
+      try {
+        leftVa = (window.screen.width - parseInt(wVal, 10)) / 2;
+        topVa = (window.screen.height - parseInt(hVal, 10)) / 2;
+
+        if (leftVa < 0) {
+          leftVa = 0
+        }
+
+        if (topVa < 0) {
+          topVa = 0
+        }
+      } catch (e) {}
+      size = "width=" + wVal + ",height=" + hVal + ",left=" + leftVa + ",top=" + topVa + ",";
+    } else if ("2" == funcType) {
+      var fid = url.substring(url.indexOf("&flowClassID=") + 13);
+      fid = fid.substring(0, fid.indexOf("&"));
+      if (fid == "00004501") {
+        var wpid = url.substring(url.indexOf("moduleid%3D") + 11, url.indexOf("moduleid%3D") + 11 + 32);
+        size = getStyleWidthHeight("", "", "", wpid);
+        if (size != null) url += "&rs=0&mt=0"; //已经调整了，不在需要调整大小
+        else size = "width=780,height=550,left=0,top=0,";
+
+        //取出宽度和高度，然后调整为居中
+        var wVal = size.substring(size.indexOf("width=") + 6, size.indexOf(",height="));
+        var hVal = size.substring(size.indexOf("height=") + 7, size.indexOf(",left="));
+
+        var leftVa = 0;
+        var topVa = 0;
+        try {
+          leftVa = (window.screen.width - parseInt(wVal, 10)) / 2;
+          topVa = (window.screen.height - parseInt(hVal, 10)) / 2;
+
+          if (leftVa < 0) {
+            leftVa = 0
+          }
+
+          if (topVa < 0) {
+            topVa = 0
+          }
+        } catch (e) {}
+        size = "width=" + wVal + ",height=" + hVal + ",left=" + leftVa + ",top=" + topVa + ",";
+      } else if (fid == "40029002") { //自定义查询2.0
+        var wpid = url.substring(url.indexOf("resultStyleID%3D") + 16, url.indexOf("resultStyleID%3D") + 16 + 32);
+        size = getStyleWidthHeight("", "", "", "", wpid);
+        if (size != null) url += "&rs=0&mt=0"; //已经调整了，不在需要调整大小
+        else size = "width=780,height=550,left=0,top=0,";
+
+        //取出宽度和高度，然后调整为居中
+        var wVal = size.substring(size.indexOf("width=") + 6, size.indexOf(",height="));
+        var hVal = size.substring(size.indexOf("height=") + 7, size.indexOf(",left="));
+
+        var swd = screen.availWidth;
+        var sht = screen.availHeight;
+        if (swd > 1280) {
+          wVal = swd * 0.9;
+          hVal = sht * 0.8;
+        }
+
+        var leftVa = 0;
+        var topVa = 0;
+        try {
+          leftVa = (window.screen.width - parseInt(wVal, 10)) / 2;
+          topVa = (window.screen.height - parseInt(hVal, 10)) / 2;
+
+          if (leftVa < 0) {
+            leftVa = 0
+          }
+
+          if (topVa < 0) {
+            topVa = 0
+          }
+        } catch (e) {}
+        size = "width=" + wVal + ",height=" + hVal + ",left=" + leftVa + ",top=" + topVa + ",";
+      }
+    }
+    var currentUserId = ""; //window.parent.frameElement.contentWindow.parent.getCurrentUserId();
+    var wino = window.open(url + "&openWin=T", funcIdStr + currentUserId, size + "toolbar=no,menubar=no,scrollbars=yes,resizable=yes,location=no,status=no");
+    wino.focus();
+    wino.focus();
+    try {
+      window.parent.frameElement.contentWindow.parent.setOpenWinObj(wino);
+    } catch (ee) {}
+  }
+  //直接打开
+  else {
+    //window.parent.document.getElementById("main").contentWindow.location.href = url;
+    var title = treeNodeObj.text;
+    //var workObj = window.parent.document.getElementById("work").contentWindow;
+    var workObj = window.parent.document.getElementById("rightMainFrame").contentWindow;
+    workObj.location.href = "loadFunc.do?action=workpanel&param=" + encodeURIComponent(url);
+  }
+}
+
+//取出打开样式的长宽高
+function getStyleWidthHeight(flowcode, taskid, taskType, wpid, customid) {
+  if (customid == undefined)
+    customid = "";
+  var reStr = null;
+  var url = "../../flow.do";
+
+  $.ajax({
+    async: false, //使用同步的Ajax请求
+    type: "POST",
+    url: url,
+    data: "action=getStyleInfo&flowcode=" + flowcode + "&taskid=" + taskid + "&taskType=" + taskType + "&workpubid=" + wpid + "&customid=" + customid,
+    success: function(msg) {
+      reStr = msg;
+    }
+  });
+
+  return reStr;
+}
