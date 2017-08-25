90e8fc0d0e95a8e83a9debb0dde0e4bbaf25660e
wujiaxiong <wujiaxiong@youngfriend.cn>
2017-08-25 16:20:13 +0800

    获取 新闻

--- app/news/news.js
+++ app/news/news.js
@@ -1,7 +1,21 @@
 require('./news.scss');
 var newsTmpl = require('./news.tmpl');
 var url = 'datas/news.json';
-$.getJSON(url, function(data) {
+var $contents = $('#news .news .list-content');
+$('#news-business .more').click(function() {
+  window.open('list.html?type=news-business');
+  return false;
+});
+$('#news-company .more').click(function() {
+  window.open('list.html?type=news-company');
+  return false;
+});
+$('#news-big .more').click(function() {
+  window.open('list.html?type=news-big');
+  return false;
+});
+
+Common.getJSON(url, $contents).then(function(data) {
   var html = newsTmpl.render({
     data: data.business
   });
@@ -14,16 +28,4 @@ $.getJSON(url, function(data) {
     data: data.big
   });
   $('#news-big .list-content').html(html);
-});
-$('#news-business .more').click(function() {
-  window.open('list.html?type=news-business');
-  return false;
-});
-$('#news-company .more').click(function() {
-  window.open('list.html?type=news-company');
-  return false;
-});
-$('#news-big .more').click(function() {
-  window.open('list.html?type=news-big');
-  return false;
 });
