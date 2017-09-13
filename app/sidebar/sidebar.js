1abd88508f3e7c431a932f0757339019e9dfe41d
wujiaxiong <wujiaxiong@youngfriend.cn>
2017-09-13 11:55:32 +0800

    树高度 修改

--- app/sidebar/sidebar.js
+++ app/sidebar/sidebar.js
@@ -5,4 +5,12 @@ $.getJSON('business.do?action=getUserInfo', function(userInfo) {
 }).fail(function() {
   Common.reLogin();
 });
+$('#sidebar iframe').on('load', function() {
+  $(this).contents().find('#tree').css({
+    position: 'absolute',
+    top: 0,
+    bottom: '50px',
+    height: ''
+  });
+});
 $('#sidebar iframe').attr('src', Common.rootpath + 'desktop2.1/jsp/mywork.jsp');
