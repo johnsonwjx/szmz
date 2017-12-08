56b0e2af7ae00f5e196c0028caf1ed5a597193b1
wujiaxiong <wujiaxiong@youngfriend.cn>
2017-12-08 10:54:35 +0800

    添加岗位信息rolename

--- app/sidebar/sidebar.js
+++ app/sidebar/sidebar.js
@@ -1,6 +1,6 @@
 require('./sidebar.scss');
 $.getJSON('business.do?action=getUserInfo', function(userInfo) {
-  $('#welcome h4').html('欢迎您:' + userInfo.username + ' 角色:管理员');
+  $('#welcome h4').html('欢迎您:' + userInfo.username + ' 角色:'+userInfo.rolename);
   window.userInfo = userInfo;
 }).fail(function() {
   Common.reLogin();
--- app/test.js
+++ app/test.js
@@ -1,21 +1,26 @@
-var Mustache = require('mustache');
+require('sweetalert2/dist/sweetalert2.css');
+require('./contact/contact.scss');
+var $ = require('jquery');
+var infoTmpl = require('./contact/info.tmpl');
 var data = {
-  menu: [{
-    "item": "parent",
-    "children": [{
-      "item": "sub child",
-      "children": [{
-        "item": "sub sub child"
-      }]
-    }]
-  }]
+  "name": "罗永钦",
+  "dept_code": "1010503",
+  "dept_name": "招投标部",
+  "role_code": "13",
+  "role_name": "实习生",
+  "personid": "35e3476d4dbd4c0aacb654bcc9550f7c",
+  "mobiletelephonenumber": "",
+  "corpname": "深圳市美芝装饰设计工程股份有限公司",
+  "qq": "",
+  "sex": "男",
+  "email": "",
+  "corpcode": "101",
+  "telephonenumber": "",
+  "gender": "male"
 };
-
-// var template = $("#template").html();
-// var recurse = $("#recurse").html();
-var parentTmpl = require('test/parent.tmpl');
-var childTmpl = require('test/child.tmpl');
-var rendered = parentTmpl.render(data, {
-  "recurse": childTmpl
+var html = infoTmpl.render(data);
+swal({
+  title: '<i class="fa fa-user" aria-hidden="true"></i>人员信息',
+  html: $(html),
+  showCloseButton: true
 });
-$("body").html(rendered);
