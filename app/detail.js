f45fa05b9144098e2244ab4a075196f7dcc7c432
wujiaxiong <wujiaxiong@youngfriend.cn>
2018-01-04 15:21:09 +0800

    返回顶部，去掉边框，内容高度修改

--- app/detail.js
+++ app/detail.js
@@ -10,12 +10,46 @@ if (issue_username) {
 }
 var issue_date = param.issue_date;
 $('#issue_user').html('（发布人 ：' + issue_username + '&nbsp;&nbsp;' + issue_date + '）');
+
+
+$('#content').scroll(_.debounce(function () {
+  if (this.scrollTop > 100) {
+    $('#to-top').fadeIn(250);
+  } else {
+    $('#to-top').fadeOut(250);
+  }
+}, 250));
+
+$('#to-top').click(function () {
+  $('#content').animate({
+    scrollTop: 0
+  }, 1000);
+});
+
+
+$('#detail').on('load', function () {
+  var height = 0;
+  var $children = $(this.contentWindow.document.body).children();
+  var imgMode = true;
+  $children.each(function (index, item) {
+    if (item.nodeName !== "IMG") {
+      imgMode = false;
+      return false;
+    }
+  });
+  if (imgMode) {
+    height = 1414 * $children.length;
+  } else if ($children.get(0).nodeName === "OBJECT") {
+    height = 820;
+  } else {
+    height = this.contentWindow.document.body.offsetHeight + 100;
+  }
+  this.style.height = height + 'px';
+});
+
 var fileUrl = Common.rootpath + '/common/updownfile/jsp/updownfile.do?action=ConvertDocToHtmlandDownFile&downWay=1&forceHtml=1&fileId=' + file_id;
 $('#detail').attr('src', fileUrl);
 
-$('#detail').on('load', function () {
-  $(this).height($('#content').height() - $('#issue_user').height() - $('#affix').height() - 30);
-});
 
 if (affix_fileid) {
   $.getJSON('business.do?action=getFileName&affix_fileid=' + affix_fileid).then(function (data) {
